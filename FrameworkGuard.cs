c743d8809a8c2a5f8d3f95290139fba08d0acc8b
Bart Koelman <bartkoelman@gmail.com>
2019-09-29 12:48:39 +0200

    Better error message when string is null, empty or whitespace.

--- FrameworkGuard.cs
+++ FrameworkGuard.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
@@ -22,13 +24,26 @@ namespace RoslynTestFramework
 
         [AssertionMethod]
         [ContractAnnotation("value: null => halt")]
-        public static void NotNullNorWhiteSpace([CanBeNull] string value, [NotNull] [InvokerParameterName] string name)
+        public static void NotNullNorEmpty<T>([CanBeNull] [ItemCanBeNull] IEnumerable<T> value,
+            [NotNull] [InvokerParameterName] string name)
         {
             NotNull(value, name);
 
+            if (!value.Any())
+            {
+                throw new ArgumentException($"'{name}' cannot be empty.", name);
+            }
+        }
+
+        [AssertionMethod]
+        [ContractAnnotation("value: null => halt")]
+        public static void NotNullNorWhiteSpace([CanBeNull] string value, [NotNull] [InvokerParameterName] string name)
+        {
             if (string.IsNullOrWhiteSpace(value))
             {
-                throw new ArgumentException($"'{name}' cannot be empty or contain only whitespace.", name);
+                NotNullNorEmpty(value, name);
+
+                throw new ArgumentException($"'{name}' cannot contain only whitespace.", name);
             }
         }
     }
